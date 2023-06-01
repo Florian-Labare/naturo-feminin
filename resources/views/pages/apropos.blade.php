f14016bc94c951352951005abf56238eded65cad
Florian <florian.labare@tech-incube.fr>
2023-06-01 19:01:42 +0200

    prestation/particuliers et vos enjeux

---
+++ resources/views/pages/apropos.blade.php
@@ -0,0 +1,25 @@
+<section class="section-prestations p-5 mt-4 max-w-7xl mx-auto md:mt-10">
+    <div id="qui-sommes-nous-trigger"></div>
+    <img class="apropos-title" src="{{url('images/apropos.png')}}" alt="" data-aos="fade-left">
+    <div id="apropos">
+        <h2 class="notre-reponse-title text-sm font-semibold uppercase lg:text-lg md:items-center lg:mr-5" data-aos="fade-right">
+           Qui sommes-nous ?
+        </h2>
+        <div class="qui-sommes-nous-container">
+            <p class="qui-sommes-nous-paragraphe italic">
+                Ancienne juriste en droit immobilier, j’ai souhaité me réorienter en 2015
+                et ai multiplié dès lors les formations autour du bien-être : naturopathie,
+                reflexologie et autres techniques manuelles, micronutrition.
+            </p>
+            <div class="mon-equipe-de-partenaire">
+                <h2></h2>
+            </div>
+            <div class="mes-outils">
+                <h2></h2>
+            </div>
+            <div class="mes-convictions">
+                <h2></h2>
+            </div>
+        </div>
+    </div>
+</section>
--- resources/views/pages/enjeux.blade.php
+++ resources/views/pages/enjeux.blade.php
@@ -1,14 +1,14 @@
 <section class="section-prestations p-5 mt-4 max-w-7xl mx-auto md:mt-10">
-    <div id="enjeux-trigger"></div>
-    <img class="prestation-title" src="{{url('images/enjeux.png')}}" alt="">
+    <img class="prestation-title" src="{{url('images/enjeux.png')}}" alt="" data-aos="fade-left">
     <div id="vos-enjeux">
+        <div id="enjeux-trigger"></div>
         <h2 class="notre-reponse-title text-sm font-semibold uppercase lg:text-lg md:items-center lg:mr-5" data-aos="fade-right">
             Notre réponse
         </h2>
         <div class="vos-enjeux-container flex flex-col lg:flex-row lg:justify-between">
             <div class="prevention-accompagnement" data-aos="fade-down">
                 <h2 class="prevention-accompagnement-title font-semibold mb-5">Prévention et accompagnement</h2>
-                <ul class="prevention-accompagnement-list">
+                <ul class="prevention-accompagnement-list uppercase">
                     <li>RPS : les risques psycho-sociaux</li>
 
                     <li>TMS: les troubles musculo-squelettiques</li>
@@ -26,15 +26,15 @@
             </div>
             <div class="line-separator line-separator-mosaique" data-aos="fade-down"></div>
             <div class="mosaique-grid text-white font-semibold" data-aos="fade-right">
-                <div class="item-mosaique-grid rounded-bl-lg"><div>Limiter l’absentéisme</div></div>
-                <div class="item-mosaique-grid rounded-bl-lg"><div>Fidéliser vos collaborateurs</div></div>
-                <div class="item-mosaique-grid rounded-bl-lg"><div>Optimiser la productivité et la performance professionnelle</div></div>
-                <div class="item-mosaique-grid rounded-bl-lg"><div>Développer une marque employeur</div></div>
-                <div class="item-mosaique-grid rounded-bl-lg"><div>Répondre au besoin de sens au travail</div></div>
-                <div class="item-mosaique-grid rounded-bl-lg"><div>Rendre les collaborateurs acteurs de la gestion de leurs émotions et du stress</div></div>
-                <div class="item-mosaique-grid rounded-bl-lg"><div>Favoriser la motivation et l’engagement</div></div>
-                <div class="item-mosaique-grid rounded-bl-lg"><div>Conciliation vie privée / vie professionnelle</div></div>
-                <div class="item-mosaique-grid rounded-bl-lg"><div>Favoriser votre attractivité en vue d’attirer de nouveaux talents</div></div>
+                <div class="item-mosaique-grid"><div>Limiter l’absentéisme</div></div>
+                <div class="item-mosaique-grid"><div>Fidéliser vos collaborateurs</div></div>
+                <div class="item-mosaique-grid"><div>Optimiser la productivité et la performance professionnelle</div></div>
+                <div class="item-mosaique-grid"><div>Développer une marque employeur</div></div>
+                <div class="item-mosaique-grid"><div>Répondre au besoin de sens au travail</div></div>
+                <div class="item-mosaique-grid"><div>Rendre les collaborateurs acteurs de la gestion de leurs émotions et du stress</div></div>
+                <div class="item-mosaique-grid"><div>Favoriser la motivation et l’engagement</div></div>
+                <div class="item-mosaique-grid"><div>Conciliation vie privée / vie professionnelle</div></div>
+                <div class="item-mosaique-grid"><div>Favoriser votre attractivité en vue d’attirer de nouveaux talents</div></div>
             </div>
         </div>
     </div>
